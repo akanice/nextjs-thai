651c1d1cf9fc81e2c66d0f76751771f900c23f0c
Thai Nguyen <nguyenhongthai1988@gmail.com>
2022-03-24 21:48:50 +0700

    upload frontend

--- pages/register/registerStep2.js
+++ pages/register/registerStep2.js
@@ -3,44 +3,44 @@ import { Button, Modal, Box } from '@material-ui/core';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import {MdCheckCircleOutline} from "react-icons/md";
-import {FaExclamationCircle} from 'react-icons/fa';
+import { MdCheckCircleOutline } from 'react-icons/md';
+import { FaExclamationCircle } from 'react-icons/fa';
 import ProgressStepper from '@components/Elements/ProgressStepper';
 import { Router } from '@mui/icons-material';
 import { useRouter } from 'next/router';
-
+import FileUpload from '@components/Upload/FileUpload';
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  border: '1px solid #ccc',
-  boxShadow: 24,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    border: '1px solid #ccc',
+    boxShadow: 24
 };
 
 const RegisterStep2 = () => {
-  const router = useRouter()
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const router = useRouter();
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  const [openModal, setOpenModal] = useState(false);
-  const openModalGuide = () => setOpenModal(true);
-  const closeModalGuide = () => setOpenModal(false);
-  const nextPage = (e) => {
-    router.push('/register')
-  } 
-  return (
-    <div className={`p-6`}>
-      <div className={``}>
-        <div className="container mx-auto py-6">
-          <ProgressStepper page={2} />
-
+    const [openModal, setOpenModal] = useState(false);
+    const openModalGuide = () => setOpenModal(true);
+    const closeModalGuide = () => setOpenModal(false);
+    const nextPage = (e) => {
+        router.push('/register');
+    };
+    return (
+        <div className={`p-6`}>
+            <div className={``}>
+                <div className="container mx-auto py-6">
+                    <ProgressStepper page={2} />
+                </div>
+            </div>
+            <FileUpload></FileUpload>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RegisterStep2;
